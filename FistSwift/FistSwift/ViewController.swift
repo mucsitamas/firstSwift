6e5235109ae3e11258f8deeca4fd9ec37da30e7b
mucsi.tamas <mucsi.tamas@wwdh.hu>
2017-08-21 19:41:53 +0200

    initial commit

---
+++ FistSwift/FistSwift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FistSwift
+//
+//  Created by Mucsi Tamás on 2017. 08. 21..
+//  Copyright © 2017. Mucsi Tamás. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Hello World")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
